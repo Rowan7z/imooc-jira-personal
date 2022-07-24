54ec7534eff0bbbc9db5b4b6ae4e6bb36740ff41
zhonglingyun <zhonglingyun@bytedance.com>
2022-07-24 23:30:14 +0800

    feat: 添加工程列表

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
-import { List } from './list'
-import { SearchPanel } from './search-panel'
+import React, { useState, useEffect } from "react";
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { cleanObject } from "../../utils";
+import * as qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+console.log("apiUrl: ", apiUrl);
 
 export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+
+  useEffect(() => {
+    fetch(
+      // `${apiUrl}/projects?name=${param.name}personId=&${param.personId}`
+      `${apiUrl}/projects?${qs.stringify(cleanObject(param))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [param]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+
   return (
     <div>
-        <SearchPanel />
-        <List />
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List users={users} list={list} />
     </div>
-  )
-}
+  );
+};
--- src/screens/project-list/list.jsx
+++ src/screens/project-list/list.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
-export const List = () => {
+export const List = ({ users, list }) => {
   return (
-    <table>list</table>
-  )
-}
+    <table>
+      <thead>
+        <tr>
+          <th>名称</th>
+          <th>负责人</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map((project) => (
+          <tr key={project.id}>
+            <td>{project.name}</td>
+            <td>
+              {users.find((user) => user.id === project.personId)?.name ||
+                "未知"}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
