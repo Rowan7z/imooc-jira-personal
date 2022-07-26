bd54566bf193fe177bc4d79e5aa99f5805bd6f43
zhonglingyun <zhonglingyun@bytedance.com>
2022-07-27 00:14:31 +0800

    feat: 添加 useMount 和 useDebounce 两个 hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
-import { cleanObject } from "../../utils";
+import { cleanObject, useDebounce, useMount } from "../../utils";
 import * as qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL;
@@ -14,25 +14,26 @@ export const ProjectListScreen = () => {
     name: "",
     personId: "",
   });
+  const debouncedParam = useDebounce(param, 2000);
 
   useEffect(() => {
     fetch(
       // `${apiUrl}/projects?name=${param.name}personId=&${param.personId}`
-      `${apiUrl}/projects?${qs.stringify(cleanObject(param))}`
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
     ).then(async (response) => {
       if (response.ok) {
         setList(await response.json());
       }
     });
-  }, [param]);
+  }, [debouncedParam]);
 
-  useEffect(() => {
+  useMount(() =>
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
-    });
-  }, []);
+    })
+  );
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+
+// 如果 value 为 null 或者 undefined，!value 返回 true
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 // 在一个函数里，改变传入的对象本身是不好的
@@ -7,9 +10,29 @@ export const cleanObject = (obj) => {
 
   Object.keys(result).forEach((key) => {
     const value = result[key];
+    // 如果 value 为 null 或者 undefined，就应该删除这个 param，不向后端传
     if (isFalsy(value)) {
       delete result[key];
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在 value 变化后设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 如果 useEffect return 的是一个函数，那么该函数会在 useEffect 上次调用完成后，下一次调用开始前执行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
 };
